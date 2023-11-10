a436e7a7fb78abb58f6496040612da16edc625c4
Arthur Antonov <artur-antonov-1992@mail.ru>
2023-11-10 17:46:05 +0300

    исправил регулярку на email

--- src/utils/constants.js
+++ src/utils/constants.js
@@ -1,15 +1,15 @@
 // Константы для отображения карточек с фильмами на странице,
 //по клике на кнопку еще
-const COUNT_MOVIES_DESKTOP = 3
-const COUNT_MOVIES_TABLET = 2
-const COUNT_MOVIES_MOBILE = 2
+const COUNT_MOVIES_DESKTOP = 3;
+const COUNT_MOVIES_TABLET = 2;
+const COUNT_MOVIES_MOBILE = 2;
 
 // Значение, которое используется
 // в фильтре длительности фильмов по времени
-const MAX_SHORT_MOVIE_DURATION = 40
+const MAX_SHORT_MOVIE_DURATION = 40;
 
 // Регулярное выражение
-const EMAIL_PATTERN = "^[a-zA-Z0-9._%+-]+@[a-zA-Z0-9.-]+\\.[a-zA-Z]{2,}$"
+const EMAIL_PATTERN = "[a-zA-Z0-9_.]+@[a-zA-Z0-9_]+\\.[a-z]{2,}";
 
 export {
   COUNT_MOVIES_DESKTOP,
@@ -17,4 +17,4 @@ export {
   COUNT_MOVIES_MOBILE,
   MAX_SHORT_MOVIE_DURATION,
   EMAIL_PATTERN,
-}
+};
